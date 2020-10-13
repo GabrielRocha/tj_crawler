8c1293c06a17ac0d7054faa8a6c9cb90b48f67e0
GabrielRocha <gabriel.rocha.gbr@gmail.com>
2020-10-13 14:58:22 -0300

    feat: set api title and description

--- crawler_api/main.py
+++ crawler_api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from crawler_api.models.requests import LegalProcess
+
+app = FastAPI(
+    title='Legal Process Crawler',
+    description=(
+        'It is a simple API to get legal process detail on TJAL or TJMS website '
+        'and convert the search result HTML to JSON'
+    )
+)
 
 
-@app.post("/")
-async def index():
+@app.post("/legal-process")
+async def show_legal_process_detail(legal_process: LegalProcess):
     return {"message": "Hello World"}
