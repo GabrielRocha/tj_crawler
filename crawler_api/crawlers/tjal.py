c7be5bd0aa7b221fd70886b506c688843578e6aa
GabrielRocha <gabriel.rocha.gbr@gmail.com>
2020-10-14 01:01:28 -0300

    feat: parse parties_involved

--- crawler_api/crawlers/tjal.py
+++ crawler_api/crawlers/tjal.py
@@ -15,6 +15,7 @@ class TJALCrawler(BaseCrawler):
 
     def parse(self, data):
         details = self.parse_legal_process_detail(data.xpath("//table[@class='secaoFormBody'][ @id='']")[0])
+        details['parties_involved'] = self.parse_parties_involved(data.xpath('//table[@id="tableTodasPartes"]'))
         return details
 
     def parse_legal_process_detail(self, data):
@@ -32,3 +33,26 @@ class TJALCrawler(BaseCrawler):
             "judge": judge,
             "value": value and value.replace("  ", "")
         }
+
+    def parse_parties_involved(self, trs):
+        parties_involved = []
+        for tr in trs.css('tr'):
+            td_label, td_value = tr.xpath('./td')
+            type_ = td_label.xpath('./span/text()').get()
+            td_value.xpath('br').remove()
+            all_texts = list(map(lambda x: x and str(x).strip(), td_value.xpath('text()').getall()))
+            all_types = list(map(lambda x: x and str(x).strip(), td_value.xpath('span/text()').getall()))
+            representatives = [
+                {
+                    "type": item and item.replace(":", ""),
+                    "name": all_texts[index]
+                }
+                for index, item in enumerate(all_types, start=1)
+            ]
+            item = {
+                'type': type_ and type_.strip().replace(":", ""),
+                'name': (all_texts and all_texts[0]) or None,
+                "representatives": representatives
+            }
+            parties_involved.append(item)
+        return parties_involved
--- tests/crawler/test_tjal.py
+++ tests/crawler/test_tjal.py
@@ -16,6 +16,13 @@ def tjal_result_html():
 
 
 @pytest.fixture
+def tjal_parties_involved_html():
+    path = os.path.dirname(__file__)
+    with open(f'{path}/fixtures/tjal_parties_involved.html') as f:
+        return Selector(text=f.read()).css("table")[0]
+
+
+@pytest.fixture
 def tjal_detail_html():
     path = os.path.dirname(__file__)
     with open(f'{path}/fixtures/tjal_detail_legal_process.html') as f:
@@ -57,6 +64,50 @@ def test_tjalcrawler_parse_legal_process_detail_return_empty_value(tjal_crawler)
     assert expected_result == result
 
 
+def test_tjalcrawler_parse_parties_involved(tjal_crawler, tjal_parties_involved_html):
+    result = tjal_crawler.parse_parties_involved(tjal_parties_involved_html)
+    expected_result = [
+        {
+            'type': 'Autor',
+            'name': 'José Carlos Cerqueira Souza Filho',
+            'representatives': [
+                {
+                    'type': 'Advogado',
+                    'name': 'Vinicius Faria de Cerqueira'
+                }
+            ]
+        },
+        {
+            'type': 'Ré',
+            'name': 'Cony Engenharia Ltda.',
+            'representatives': [
+                {
+                    'type': 'Advogado',
+                    'name': 'Marcus Vinicius Cavalcante Lins Filho'
+                },
+                {
+                    'type': 'Advogado',
+                    'name': 'Thiago Maia Nobre Rocha'
+                },
+            ]
+        },
+    ]
+    assert expected_result == result
+
+
+def test_tjalcrawler_parse_parties_involved_return_empty_value(tjal_crawler):
+    selector = Selector(text='<table><tbody><tr><td></td><td></td></tr></tbody></table>')
+    result = tjal_crawler.parse_parties_involved(selector)
+    expected_result = [
+        {
+            'type': None,
+            'name': None,
+            'representatives': []
+        }
+    ]
+    assert expected_result == result
+
+
 def test_tjalcrawler_parse(tjal_crawler, tjal_result_html):
     result = tjal_crawler.parse(tjal_result_html)
     expected_result = {
@@ -65,6 +116,32 @@ def test_tjalcrawler_parse(tjal_crawler, tjal_result_html):
         'subject': 'Dano Material',
         'distribution': '02/05/2018 às 19:01 - Sorteio',
         'judge': 'José Cícero Alves da Silva',
-        'value': 'R$ 281.178,42'
+        'value': 'R$ 281.178,42',
+        'parties_involved': [
+            {
+                'type': 'Autor',
+                'name': 'José Carlos Cerqueira Souza Filho',
+                'representatives': [
+                    {
+                        'type': 'Advogado',
+                        'name': 'Vinicius Faria de Cerqueira'
+                    }
+                ]
+            },
+            {
+                'type': 'Ré',
+                'name': 'Cony Engenharia Ltda.',
+                'representatives': [
+                    {
+                        'type': 'Advogado',
+                        'name': 'Marcus Vinicius Cavalcante Lins Filho'
+                    },
+                    {
+                        'type': 'Advogado',
+                        'name': 'Thiago Maia Nobre Rocha'
+                    },
+                ]
+            },
+        ]
     }
     assert expected_result == result
